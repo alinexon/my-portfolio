68e27a8dd0eda011864ab153653f760ac1c5d61a
alinexon <alinexon55@gmail.com>
2025-05-01 23:30:19 +0430

    add fixed size

--- src/components/left-sidebar.tsx
+++ src/components/left-sidebar.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { Avatar, AvatarImage } from "./ui/avatar";
 import { Menu } from "lucide-react";
 import { Button } from "./ui/button";
@@ -32,11 +32,12 @@ function LeftSidebar() {
       {/* Left Sidebar */}
       <aside
         className={cn(
-          "fixed top-0 left-0 h-full w-64 bg-white shadow-lg z-40 transition-transform duration-300",
+          " top-0 left-0 h-full w-64 bg-white shadow-lg z-40 transition-transform duration-300 absolute",
           "lg:translate-x-0",
           isOpen ? "translate-x-0" : "-translate-x-full",
           "lg:block"
         )}
+        style={{ width: "16rem" }}
       >
         {/* Sticky Header */}
         <div className="sticky top-0 z-10 bg-white p-6 border-b">
@@ -76,182 +77,6 @@ function LeftSidebar() {
                 </ul>
               </div>
             </div>
-            <div className="flex flex-row gap-5">
-              <div>
-                <ul>
-                  <li>Residence:</li>
-                  <li>City:</li>
-                  <li>Age:</li>
-                </ul>
-              </div>
-              <div dir="rtl" className="text-gray-600">
-                <ul>
-                  <li>Afghanistan</li>
-                  <li>Kabul</li>
-                  <li>28</li>
-                </ul>
-              </div>
-            </div>{" "}
-            <div className="flex flex-row gap-5">
-              <div>
-                <ul>
-                  <li>Residence:</li>
-                  <li>City:</li>
-                  <li>Age:</li>
-                </ul>
-              </div>
-              <div dir="rtl" className="text-gray-600">
-                <ul>
-                  <li>Afghanistan</li>
-                  <li>Kabul</li>
-                  <li>28</li>
-                </ul>
-              </div>
-            </div>{" "}
-            <div className="flex flex-row gap-5">
-              <div>
-                <ul>
-                  <li>Residence:</li>
-                  <li>City:</li>
-                  <li>Age:</li>
-                </ul>
-              </div>
-              <div dir="rtl" className="text-gray-600">
-                <ul>
-                  <li>Afghanistan</li>
-                  <li>Kabul</li>
-                  <li>28</li>
-                </ul>
-              </div>
-            </div>{" "}
-            <div className="flex flex-row gap-5">
-              <div>
-                <ul>
-                  <li>Residence:</li>
-                  <li>City:</li>
-                  <li>Age:</li>
-                </ul>
-              </div>
-              <div dir="rtl" className="text-gray-600">
-                <ul>
-                  <li>Afghanistan</li>
-                  <li>Kabul</li>
-                  <li>28</li>
-                </ul>
-              </div>
-            </div>{" "}
-            <div className="flex flex-row gap-5">
-              <div>
-                <ul>
-                  <li>Residence:</li>
-                  <li>City:</li>
-                  <li>Age:</li>
-                </ul>
-              </div>
-              <div dir="rtl" className="text-gray-600">
-                <ul>
-                  <li>Afghanistan</li>
-                  <li>Kabul</li>
-                  <li>28</li>
-                </ul>
-              </div>
-            </div>{" "}
-            <div className="flex flex-row gap-5">
-              <div>
-                <ul>
-                  <li>Residence:</li>
-                  <li>City:</li>
-                  <li>Age:</li>
-                </ul>
-              </div>
-              <div dir="rtl" className="text-gray-600">
-                <ul>
-                  <li>Afghanistan</li>
-                  <li>Kabul</li>
-                  <li>28</li>
-                </ul>
-              </div>
-            </div>{" "}
-            <div className="flex flex-row gap-5">
-              <div>
-                <ul>
-                  <li>Residence:</li>
-                  <li>City:</li>
-                  <li>Age:</li>
-                </ul>
-              </div>
-              <div dir="rtl" className="text-gray-600">
-                <ul>
-                  <li>Afghanistan</li>
-                  <li>Kabul</li>
-                  <li>28</li>
-                </ul>
-              </div>
-            </div>{" "}
-            <div className="flex flex-row gap-5">
-              <div>
-                <ul>
-                  <li>Residence:</li>
-                  <li>City:</li>
-                  <li>Age:</li>
-                </ul>
-              </div>
-              <div dir="rtl" className="text-gray-600">
-                <ul>
-                  <li>Afghanistan</li>
-                  <li>Kabul</li>
-                  <li>28</li>
-                </ul>
-              </div>
-            </div>{" "}
-            <div className="flex flex-row gap-5">
-              <div>
-                <ul>
-                  <li>Residence:</li>
-                  <li>City:</li>
-                  <li>Age:</li>
-                </ul>
-              </div>
-              <div dir="rtl" className="text-gray-600">
-                <ul>
-                  <li>Afghanistan</li>
-                  <li>Kabul</li>
-                  <li>28</li>
-                </ul>
-              </div>
-            </div>{" "}
-            <div className="flex flex-row gap-5">
-              <div>
-                <ul>
-                  <li>Residence:</li>
-                  <li>City:</li>
-                  <li>Age:</li>
-                </ul>
-              </div>
-              <div dir="rtl" className="text-gray-600">
-                <ul>
-                  <li>Afghanistan</li>
-                  <li>Kabul</li>
-                  <li>28</li>
-                </ul>
-              </div>
-            </div>{" "}
-            <div className="flex flex-row gap-5">
-              <div>
-                <ul>
-                  <li>Residence:</li>
-                  <li>City:</li>
-                  <li>Age:</li>
-                </ul>
-              </div>
-              <div dir="rtl" className="text-gray-600">
-                <ul>
-                  <li>Afghanistan</li>
-                  <li>Kabul</li>
-                  <li>28</li>
-                </ul>
-              </div>
-            </div>{" "}
           </div>
         </div>
       </aside>
