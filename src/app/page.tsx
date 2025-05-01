f7520876a9dc2f60c1b64c965906127e1841f5cb
alinexon <alinexon55@gmail.com>
2025-05-01 23:29:42 +0430

    add fixed size

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,18 @@
 "use client";
 
 import LeftSidebar from "@/components/left-sidebar";
-import MainContant from "@/components/main-content";
+import MainContent from "@/components/main-content";
 import RightSidebar from "@/components/right-sidebar";
-import React, { useState } from "react";
+import { useState } from "react";
 
 function Page() {
   const [isRightSidebarOpen, setIsRightSidebarOpen] = useState(false);
 
   return (
-    <div className="flex min-h-screen relative">
+    <div className="flex min-h-screen max-w-[1500px] mx-auto relative">
       <LeftSidebar />
       <div className="flex-1 transition-all duration-300">
-        <MainContant isRightSidebarOpen={isRightSidebarOpen} />
+        <MainContent isRightSidebarOpen={isRightSidebarOpen} />
       </div>
       <RightSidebar onToggle={setIsRightSidebarOpen} />
     </div>
@@ -20,48 +20,3 @@ function Page() {
 }
 
 export default Page;
-
-// "use client";
-
-// import LeftSidebar from "@/components/left-sidebar";
-// import MainContant from "@/components/main-content";
-// import RightSidebar from "@/components/right-sidebar";
-// import React, { useState } from "react";
-
-// function Page() {
-//   const [isRightSidebarOpen, setIsRightSidebarOpen] = useState(false);
-
-//   return (
-//     <div className="flex min-h-screen">
-//       <div className="w-16 lg:w-16 bg-gray-100">
-//         <LeftSidebar />
-//       </div>
-
-//       <div className="flex-1 transition-all duration-300">
-//         <MainContant isRightSidebarOpen={isRightSidebarOpen} />
-//       </div>
-
-//       <RightSidebar onToggle={setIsRightSidebarOpen} />
-//     </div>
-//   );
-// }
-
-// export default Page;
-
-// import LeftSidebar from "@/components/left-sidebar";
-// import MainContant from "@/components/main-content";
-// import RightSidebar from "@/components/right-sidebar";
-// // import { SidebarProvider } from "@/components/ui/sidebar";
-// import React from "react";
-
-// function page() {
-//   return (
-//     <div className="grid grid-cols-[16rem_1fr_16rem] min-h-screen">
-//       <LeftSidebar />
-//       <MainContant />
-//       <RightSidebar />
-//     </div>
-//   );
-// }
-
-// export default page;
