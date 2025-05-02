4092f97a952b08d77146c93b17f19eea141de6b6
alinexon <alinexon55@gmail.com>
2025-05-02 17:09:00 +0430

    mode-toggle file

---
+++ src/components/mode-toggle.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { Moon, Sun } from "lucide-react";
+import { useTheme } from "next-themes";
+
+export function ModeToggle() {
+  const { theme, setTheme } = useTheme();
+  const toggleTheme = () => {
+    setTheme(theme === "dark" ? "light" : "dark");
+  };
+
+  return (
+    <Button variant="outline" size="icon" onClick={toggleTheme}>
+      <Sun className="h-[1.2rem] w-[1.2rem] transition-all dark:hidden" />
+      <Moon className="h-[1.2rem] w-[1.2rem] transition-all hidden dark:block" />
+      <span className="sr-only">Toggle theme</span>
+    </Button>
+  );
+}
