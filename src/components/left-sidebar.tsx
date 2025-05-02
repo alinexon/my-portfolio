d19d8f8793343604eb3c99b7b15ec2f6be0ff215
alinexon <alinexon55@gmail.com>
2025-05-02 17:08:39 +0430

    add light and dark mode

--- src/components/left-sidebar.tsx
+++ src/components/left-sidebar.tsx
@@ -9,7 +9,6 @@ import { cn } from "@/lib/utils";
 function LeftSidebar() {
   const [isOpen, setIsOpen] = useState(false);
 
-  // Close on window resize to large screens
   useEffect(() => {
     const handleResize = () => {
       if (window.innerWidth >= 1024) {
@@ -24,59 +23,62 @@ function LeftSidebar() {
     <>
       {/* Toggle button - Only on small screens */}
       <div className="fixed top-4 left-4 z-50 lg:hidden">
-        <Button variant="ghost" size="icon" onClick={() => setIsOpen(!isOpen)}>
+        <Button variant="ghost" size="icon" onClick={() => setIsOpen(true)}>
           {!isOpen && <Menu />}
         </Button>
       </div>
-      <div className="fixed top-4 left-62 z-50 lg:hidden">
+      <div className="fixed top-4 left-62 z-50 lg:hidden dark:text-white">
         <Button variant="ghost" size="icon" onClick={() => setIsOpen(false)}>
           {isOpen && <X />}
         </Button>
       </div>
 
-      {/* Left Sidebar */}
+      {/* Sidebar */}
       <aside
         className={cn(
-          " top-0 left-0 h-full  bg-white shadow-lg z-40 transition-transform duration-300 absolute",
+          "top-0 left-0 h-full bg-white dark:bg-zinc-900 shadow-md dark:shadow-lg transition-transform duration-300 absolute z-40 lg:block",
           "lg:translate-x-0",
-          isOpen ? "translate-x-0" : "-translate-x-full",
-          "lg:block"
+          isOpen ? "translate-x-0" : "-translate-x-full"
         )}
         style={{ width: "18rem" }}
       >
-        <div className="sticky top-0 z-10 bg-white p-6 border-b">
+        {/* Header */}
+        <div className="sticky top-0 z-10 bg-white dark:bg-zinc-900 border-b border-gray-200 dark:border-zinc-800 p-6">
           <div className="flex flex-col items-center relative">
-            <Avatar className="h-24 w-24 mb-4 relative">
+            <Avatar className="h-24 w-24 mb-4">
               <AvatarImage src="/images/profileimg.png" alt="Profile photo" />
               <AvatarFallback>Ahmad</AvatarFallback>
             </Avatar>
-
-            {/* Hover on dot  */}
             <div className="absolute group">
               <div className="absolute top-18 left-7 h-4 w-4 bg-yellow-500 rounded-full cursor-pointer"></div>
-              <div className="absolute left-14 top-18 hidden group-hover:flex z-50 bg-white p-2 rounded-md opacity-15 shadow-md whitespace-pre">
-                <span className="text-xs text-gray-800">
+
+              <div className="absolute left-14 top-18 hidden group-hover:flex z-50 bg-white dark:bg-zinc-800 p-2 rounded-md shadow-md whitespace-pre">
+                <span className="text-xs text-gray-800 dark:text-gray-100">
                   <pre>I am available for </pre>
                   freelance hire
                 </span>
               </div>
-              <div className="absolute left-8 top-28 hidden group-hover:flex z-60 bg-white p-2 rounded-md shadow-md whitespace-nowrap">
-                <span className="text-xs text-gray-800">
+
+              {/* Second Hover Box */}
+              <div className="absolute left-8 top-28 hidden group-hover:flex z-60 bg-white dark:bg-zinc-800 p-2 rounded-md shadow-md whitespace-nowrap">
+                <span className="text-xs text-gray-800 dark:text-gray-100">
                   I am available for freelance hire
                 </span>
               </div>
             </div>
 
-            <h2 className="text-xl font-bold">Ahmad Joya</h2>
-            <p className="text-sm text-muted-foreground">
+            <h2 className="text-xl font-bold text-black dark:text-white">
+              Ahmad Joya
+            </h2>
+            <p className="text-sm text-gray-500 dark:text-gray-400">
               Full Stack Developer
             </p>
           </div>
         </div>
 
-        {/* Scrollable Content */}
+        {/* Scrollable content */}
         <div
-          className="overflow-y-auto h-[calc(100vh-232px)] p-6"
+          className="overflow-y-auto h-[calc(100vh-232px)] p-6 custom-scrollbar"
           style={{
             scrollbarWidth: "none",
             msOverflowStyle: "none",
@@ -85,14 +87,14 @@ function LeftSidebar() {
           <div className="flex flex-col items-center mb-6">
             <div className="flex flex-row gap-5">
               <div>
-                <ul>
+                <ul className="space-y-1 text-gray-700 dark:text-gray-300">
                   <li>Residence:</li>
                   <li>City:</li>
                   <li>Age:</li>
                 </ul>
               </div>
-              <div dir="rtl" className="text-gray-600">
-                <ul>
+              <div dir="rtl" className="text-gray-600 dark:text-gray-400">
+                <ul className="space-y-1">
                   <li>Afghanistan</li>
                   <li>Kabul</li>
                   <li>28</li>
--- src/components/main-content.tsx
+++ src/components/main-content.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import Image from "next/image";
+import { ModeToggle } from "./mode-toggle";
 
 type Props = {
   isRightSidebarOpen: boolean;
@@ -8,27 +9,39 @@ type Props = {
 
 function MainContent({ isRightSidebarOpen }: Props) {
   return (
-    <main
-      className={`transition-all duration-300 px-16 py-16 min-h-screen relative z-10 lg:ml-64 ${
-        isRightSidebarOpen ? "lg:translate-x-[-12rem]" : "lg:translate-x-0"
-      }`}
-    >
-      <section id="home" className="min-h-screen">
-        <div className="w-full h-16 mx-auto text-center">
-          <Image
-            src="/images/mountain.png"
-            alt="bg Photo"
-            width={1000}
-            height={300}
-            className="h-[400px] w-[1200px] opacity-50"
-          />
-          <h1 className="text-4xl font-bold mb-4">Welcome to My Portfolio</h1>
-          <p className="text-xl text-muted-foreground">
-            I build beautiful, responsive, and user-friendly web applications. I
-          </p>
+    <>
+      <header className="h-16 mr-16">
+        <div className="sm:ml-8 lg:ml-96 mt-2 ml-40 dark:sm:ml-8 dark:lg:ml-96">
+          <ModeToggle />
         </div>
-      </section>
-    </main>
+      </header>
+
+      <main
+        className={`transition-all duration-300 px-16 py-16 min-h-screen relative z-10 lg:ml-64 ${
+          isRightSidebarOpen ? "lg:translate-x-[-12rem]" : "lg:translate-x-0"
+        }`}
+      >
+        <section
+          id="home"
+          className="min-h-screen text-gray-800 dark:text-gray-100"
+        >
+          <div className="w-full h-16 mx-auto text-center">
+            <Image
+              src="/images/mountain.png"
+              alt="bg Photo"
+              width={1000}
+              height={300}
+              className="h-[400px] w-[1200px] opacity-50 mx-auto"
+            />
+            <h1 className="text-4xl font-bold mb-4">Welcome to My Portfolio</h1>
+            <p className="text-xl text-gray-500 dark:text-gray-400">
+              I build beautiful, responsive, and user-friendly web applications.
+              I
+            </p>
+          </div>
+        </section>
+      </main>
+    </>
   );
 }
 
