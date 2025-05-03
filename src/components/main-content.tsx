ecc09ece9dcb8c6b5079c5af94b8793151da0a51
alinexon <alinexon55@gmail.com>
2025-05-03 14:26:37 +0430

    add hover effect on left-sidebar photo

--- src/components/main-content.tsx
+++ src/components/main-content.tsx
@@ -5,9 +5,11 @@ import { ModeToggle } from "./mode-toggle";
 
 type Props = {
   isRightSidebarOpen: boolean;
+  showImage: boolean;
+  setShowImage: (value: boolean) => void;
 };
 
-function MainContent({ isRightSidebarOpen }: Props) {
+function MainContent({ isRightSidebarOpen, showImage, setShowImage }: Props) {
   return (
     <>
       <header className=" fixed lg:absolute top-0 left-0 w-full lg:max-w-[1500px] h-20   bg-white shadow-lg dark:bg-zinc-900 dark:shadow-2xl z-20 flex justify-center items-center">
@@ -21,6 +23,18 @@ function MainContent({ isRightSidebarOpen }: Props) {
           isRightSidebarOpen ? "lg:translate-x-[-12rem]" : "lg:translate-x-0"
         }`}
       >
+        {showImage && (
+          <div
+            className="fixed inset-0 bg-black bg-opacity-80 w-full h-full shadow-lg flex items-center justify-center z-100 cursor-pointer transition-all"
+            onClick={() => setShowImage(false)}
+          >
+            <img
+              src="/images/profileimg.png"
+              alt="Zoomed profile"
+              className="w-full h-full object-contain animate-zoomIn"
+            />
+          </div>
+        )}
         <section
           id="home"
           className="min-h-screen text-gray-800 dark:text-gray-100"
