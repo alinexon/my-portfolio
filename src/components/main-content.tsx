426e08d613a295c795e9bbd706d9b64a984ceb54
alinexon <alinexon55@gmail.com>
2025-05-04 19:11:44 +0430

    add feature in Home section

--- src/components/main-content.tsx
+++ src/components/main-content.tsx
@@ -1,7 +1,9 @@
 "use client";
 
 import Image from "next/image";
+import { TypeAnimation } from "react-type-animation";
 import { ModeToggle } from "./mode-toggle";
+import { Button } from "./ui/button";
 
 type Props = {
   isRightSidebarOpen: boolean;
@@ -10,6 +12,12 @@ type Props = {
 };
 
 function MainContent({ isRightSidebarOpen, showImage, setShowImage }: Props) {
+  const stats = [
+    { value: "10+", label: "Years Experience" },
+    { value: "143", label: "Completed Projects" },
+    { value: "114", label: "Happy Customers" },
+    { value: "20+", label: "Honors and Awards" },
+  ];
   return (
     <>
       <header className=" fixed lg:absolute top-0 left-0 w-full lg:max-w-[1500px] h-20   bg-white shadow-lg dark:bg-zinc-900 dark:shadow-2xl z-20 flex justify-center items-center">
@@ -19,7 +27,7 @@ function MainContent({ isRightSidebarOpen, showImage, setShowImage }: Props) {
       </header>
 
       <main
-        className={`transition-all duration-300 px-16 py-16 pt-32 min-h-screen relative z-10 lg:ml-64 ${
+        className={`transition-all duration-300 px-16 py-16 pt-28 min-h-screen relative z-10 lg:ml-64 ${
           isRightSidebarOpen ? "lg:translate-x-[-12rem]" : "lg:translate-x-0"
         }`}
       >
@@ -39,21 +47,79 @@ function MainContent({ isRightSidebarOpen, showImage, setShowImage }: Props) {
         )}
         <section
           id="home"
-          className="min-h-screen text-gray-800 dark:text-gray-100"
+          className=" text-gray-800 dark:text-gray-100 mt-4 flex flex-col"
         >
-          <div className="w-full h-16 mx-auto text-center">
-            <Image
-              src="/images/mountain.png"
-              alt="bg Photo"
-              width={1000}
-              height={300}
-              className="h-[400px] w-[1200px] opacity-50 mx-auto"
-            />
-            <h1 className="text-4xl font-bold mb-4">Welcome to My Portfolio</h1>
-            <p className="text-xl text-gray-500 dark:text-gray-400">
-              I build beautiful, responsive, and user-friendly web applications.
-              I
-            </p>
+          <div className="flex justify-center">
+            <div className="relative w-full max-w-[1100px] h-[350px] rounded-xl">
+              <Image
+                src="/images/mountain.png"
+                alt="Background"
+                fill
+                className="object-cover w-full h-full opacity-50"
+                priority
+              />
+              <section
+                className="
+                  relative z-10 w-full h-full flex items-center justify-center px-4 md:px-8  dark:text-white "
+              >
+                <div className="w-full grid grid-cols-1 lg:grid-cols-2 items-center">
+                  <div className="text-center lg:text-left">
+                    <h1 className="text-3xl sm:text-4xl md:text-5xl font-bold mb-6 leading-tight">
+                      Discover my Amazing <br /> Art Space!
+                    </h1>
+                    <p className="text-lg sm:text-sm md:text-2xl font-semibold text-gray-700 dark:text-white">
+                      &lt;<span className="text-yellow-300 ">code</span>&gt;I
+                      build
+                      <TypeAnimation
+                        sequence={[
+                          " design mockups.",
+                          2000,
+                          "",
+                          " responsive design.",
+                          2000,
+                          "",
+                          " web application.",
+                          2000,
+                        ]}
+                        wrapper="span"
+                        cursor={true}
+                        repeat={Infinity}
+                        className="text-gray-700 dark:text-white"
+                      />
+                      &lt;<span className="text-yellow-300">/code</span>&gt;
+                    </p>
+                    <Button className="mt-6 bg-yellow-500 text-black hover:bg-yellow-600 transform transition-all duration-300 hover:-translate-y-0.5">
+                      <a href="#"> EXPLORE NOW </a>
+                    </Button>
+                  </div>
+                </div>
+              </section>
+              {/* Person Image */}
+              <div className="hidden lg:block absolute right-[-40px] top-[-90px]  ">
+                <Image
+                  src="/images/personimg.png"
+                  alt="Person Image"
+                  width={500}
+                  height={500}
+                  className="h-[440px] w-auto object-contain rounded-md"
+                />
+              </div>
+            </div>
+          </div>
+
+          <div className="mx-auto px-4 py-7">
+            <ul className="grid grid-cols-2 md:grid-cols-4 gap-y-6 gap-x-6 text-center">
+              {stats.map((stat, index) => (
+                <li key={index} className="flex items-center justify-center">
+                  <span className="text-xl sm:text-sm md:text-3xl font-bold text-yellow-500 dark:text-yellow-400 mr-2 sm:mr-3">
+                    {stat.value}
+                  </span>
+                  <span className="text-xs sm:text-sm md:text-base font-medium">
+                    {stat.label}
+                  </span>
+                </li>
+              ))}
+            </ul>
           </div>
         </section>
       </main>
